bef328319bd09ac7defd5c23b666042ef691fa22
Hannes Becher <hannesbecher@gmx.de>
2023-10-26 09:40:18 +0100

    code

--- wettstein_polypl.R
+++ wettstein_polypl.R
@@ -1,5 +1,5 @@
 
-dat <- read.table("~/temp/wettstein_polypl.csv",
+dat <- read.table("~/git_repos/wettstein1932//wettstein_polypl.csv",
                   sep="\t",
                   header=T
                   )
@@ -22,3 +22,36 @@ legend("bottomright",
        legend=levels(as.factor(dat$Taxon)),
        lwd=2
        )
+
+
+dat2 <- dat[!(dat$Taxon %in% c("PiHy", "PhHy")),]
+
+
+head(dat2)
+lm01 <- lm(y ~ x + Taxon,
+           data=dat2)
+summary(lm01)
+par(mfrow=c(2,2))
+par(mfrow=c(1,1))
+plot(lm01)
+lm02 <- lm(log(y) ~ x + Taxon,
+           data=dat2)
+summary(lm02)
+plot(lm02)
+
+lm03 <- lm(log(y) ~ x * Taxon,
+           data=dat2)
+summary(lm03)
+plot(lm03)
+table(dat2$Taxon)
+
+plot(y~x,
+     log="y",
+     data=dat2,
+     col=factor(dat2$Taxon))
+dat2[which.max(dat2$y),]
+dat2 <- dat2[-which.max(dat2$y),]
+plot(y~x,
+     log="y",
+     data=dat2,
+     col=factor(dat2$Taxon))
